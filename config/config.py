8e3ccf3fe9737c659a20fd0e4785e178f946c6e7
acgbfull <acgb@protonmail.com>
2018-06-17 10:56:19 +0800

    配置文件

---
+++ config/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+配置文件
+"""
+
+# 超时时间 (overtime time)
+timeout = 5
+
+# nmap运行时的默认值 （Nmap command settings）
+nmap_cmd_line = "{0}".format("-sS -sV --top-ports 10")
